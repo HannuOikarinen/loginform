a3ce2b55ae15a217e8e5f7dcb7fbfc2685710d25
HannuOikarinen <oikarhan@gmail.com>
2024-01-27 20:52:06 +0200

    viikkotehtava3

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Login from './screens/login';
+import { PaperProvider } from 'react-native-paper';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider>
+      <Login />
+    </PaperProvider>
   );
 }
 
@@ -16,5 +18,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+  },
 });
